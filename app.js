b8a2e720c7dc2f2e0df4955e18cf3ab5e3db999c
Scythe1224 <reliablesoft4@gmail.com>
2026-05-06 18:54:05 +0530

    Revert "Initialize login before dashboard render"
    This reverts commit 6ec815be0a4af76e7fa48959758b75073cb4fbc2.

--- app.js
+++ app.js
@@ -2956,9 +2956,5 @@ document.addEventListener('keydown',e=>{
     closeClientsFlyout();
   }
 });
+renderAll();
 initializeLogin();
-try{
-  renderAll();
-}catch(_err){
-  console.error('Dashboard bootstrap render failed', _err);
-}
